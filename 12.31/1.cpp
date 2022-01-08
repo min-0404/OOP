630cb525bb949d7189dfb4b2cf964957f525a64c
Minseok Kim <minsuck040484@gmail.com>
2022-01-08 13:40:49 +0900

    .

--- 12.31/1.cpp
+++ 12.31/1.cpp
@@ -1,11 +1,12 @@
-// 5개의 "string 배열"을 선언하고 getline() 을 이용하여 문자열을 입력 받아 사전 순으로 가장 뒤에 나오는 문자열을 출력하라.
+// 5개의 "string 배열"을 선언하고 getline() 을 이용하여
+// 문자열을 입력 받아 사전 순으로 가장 뒤에 나오는 문자열을 출력하라.
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main()
 {
-    string names[5]; //문자열 배열 선언
+    string names[5]; // "문자열 배열" 선언
     for(int i = 0; i < 5; i++){
         cout << "이름";
         getline(cin, names[i], '\n'); // '\n' 은 생략가능 : 디폴트가 엔터임
